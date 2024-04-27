55ca41d124027a82dc50bb1ed8ef9da59b905171
bcblevins <beaucblevins@gmail.com>
2024-04-27 19:29:47 -0400

    First Commit

---
+++ src/test/java/com/bcb/vetra/VetraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcb.vetra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
